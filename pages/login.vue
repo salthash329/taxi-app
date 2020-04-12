9390dd2d421aa8f6185a5fbafcc46a01ee010b94
salthash329 <salt.hash329@gmail.com>
2020-04-12 15:02:17 +0900

    First Commit

---
+++ pages/login.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-layout>
+    <v-flex class="text-center">
+      <blockquote class="blockquote">
+        &#8220;ログインページ.&#8221;
+        <footer>
+          <small>
+          </small>
+        </footer>
+      </blockquote>
+    </v-flex>
+  </v-layout>
+</template>
